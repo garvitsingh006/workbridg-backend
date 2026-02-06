f8dcfa1441940c748d457a159706c8c5e0b2e934
Garvit Singh <garvitsingh006@gmail.com>
2026-02-06 21:15:44 +0530

    Added head seperately in health router

--- src/routes/health.route.js
+++ src/routes/health.route.js
@@ -3,6 +3,7 @@ import { health } from "../controllers/health.controller.js";
 
 const router = Router();
 
-router.route("/").get(health).head((req,res) => res.sendStatus(200));
+router.route("/").get(health)
+router.route("/").head((req,res) => res.sendStatus(200));
 
 export default router;
